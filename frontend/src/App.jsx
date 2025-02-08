980e9c8acab7626853ca352204db6af12c93a57e
neilmd <neil.capistrano2007@gmail.com>
2025-02-08 00:55:33 -0700

    Migrated from v3 to v4 Tailwind, npm to pnpm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {Button} from './components/ui/button';
-import './App.css'
+import {Button} from '@/components/ui/button';
+import '@/App.css'
 import { CarouselDemo } from './components/demo/CarouselDemo';
 
 
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <Button>Hello,</Button>
+      <Button variant="destructive">Button</Button>
       <CarouselDemo></CarouselDemo>
     </>
   )
