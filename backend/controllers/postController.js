1da9eabf5fb398df141b654c69f36e7a98b05484
neilmd <neil.capistrano2007@gmail.com>
2025-03-18 11:00:24 -0600

    feat: Added Edit Post API

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -31,7 +31,40 @@ module.exports = ({ asyncHandler, logger, models }) => {
     logger.info("postController/add: END");
     res.status(201).json(res.locals.objResult);
   });
-  postController.edit = asyncHandler((req, res) => {});
+  postController.edit = asyncHandler(async (req, res) => {
+    logger.info("postController/edit: START");
+
+    const filter = { _id: req.body._id }; // Find the post by ID
+    const update = {
+      authorId: req.body.authorId,
+      authorType: req.body.authorType,
+      postType: req.body.postType,
+      title: req.body.title,
+      content: req.body.content,
+      tags: req.body.tags,
+      programs: req.body.programs,
+      courses: req.body.courses,
+    };
+
+    const options = { upsert: false, new: true, setDefaultsOnInsert: true };
+    // upsert: true ensures insert if no document found
+    // new: true returns the updated document
+    // setDefaultsOnInsert ensures that default values are applied on insert
+
+    const result = await Post.findOneAndUpdate(filter, update, options);
+
+    if (result) {
+      res.locals.objResult.numCode = 0;
+      res.locals.objResult.objData = result;
+      res.locals.objResult.objSucess = "Successfully Updated";
+    } else {
+      res.locals.objResult.numCode = 1;
+      res.locals.objResult.objSucess = "Record not found";
+    }
+
+    logger.info("postController/edit: END");
+    res.status(201).json(res.locals.objResult);
+  });
   postController.delete = asyncHandler((req, res) => {});
   return postController;
 };
