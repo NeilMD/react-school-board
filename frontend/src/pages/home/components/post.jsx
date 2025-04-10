f9d47112e804ab0853c6d87b91929dcdb0f3b6cf
neilmd <neil.capistrano2007@gmail.com>
2025-04-10 04:41:57 -0600

    fix

--- frontend/src/pages/home/components/post.jsx
+++ frontend/src/pages/home/components/post.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { MessageSquareTextIcon } from "lucide-react";
 import { Bookmark } from "lucide-react";
-import { Calendar, Map } from "lucide-react";
+import { Calendar, Map, BookOpen, LibraryBig } from "lucide-react";
 import Chloe from "@/assets/chloechoi.jpeg";
 import React, { useState, useRef, useEffect } from "react";
 import { faker } from "@faker-js/faker";
@@ -20,32 +20,46 @@ function Post({ postObj }) {
     location: `${faker.location.state()}, ${faker.location.country()}`,
     content: postObj.content || faker.lorem.paragraph(),
     tag: postObj?.tags[0] || faker.company.buzzNoun(),
+    userDetails: generateRandomAcronym(),
   });
+
+  function generateRandomAcronym() {
+    const options = ["IDWD", "IDGD", "IDUX"];
+
+    // Randomly select one acronym from the options array
+    const randomAcronym = options[Math.floor(Math.random() * options.length)];
+
+    return randomAcronym;
+  }
   console.log(postObj.tags[0]);
   // Define PostHeader inline
   const PostHeader = ({ postObj }) => {
     return (
       <div className="post-header flex w-full flex-row">
         <div className="flex basis-1/2 gap-2">
-          <Avatar>
+          <Avatar className="h-[50px] w-[50px]">
             <AvatarImage
-              className="object-cover"
+              className="aspect-square object-cover"
               src={postDataRef.current.avatar}
               alt="@shadcn"
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div className="flex flex-col justify-center">
-            <span className="leading-[0.8] font-normal">
+            <span className="leading-[1] font-normal">
               {postDataRef.current.name}
             </span>
-            <span className="font-lighter text-xs opacity-75">30 mins ago</span>
+            <span className="font-lighter text-xs opacity-75">
+              <span className="font-semibold text-sky-700">SADT </span>|{" "}
+              {postDataRef.current.userDetails} | 4th Semester
+            </span>
+            <span className="text-xs font-light opacity-65">Apr 10, 2025</span>
           </div>
         </div>
-        <div className="flex basis-2/3 flex-row justify-end">
+        <div className="flex h-[50px] basis-2/3 flex-row items-baseline justify-end">
           <Badge
             className={
-              "rounded-[20px] border-none bg-cyan-600 px-5 font-light text-white"
+              "flex h-fit items-center rounded-[20px] border-none bg-cyan-600 px-4 py-2 text-center font-light text-white"
             }
             variant="outline">
             #{postDataRef.current.tag}
@@ -59,21 +73,45 @@ function Post({ postObj }) {
   const PostBody = ({ postObj }) => {
     return (
       <div className="post-body">
-        <h5 className="mt-6 mb-3 text-[20px] font-bold text-cyan-600 capitalize">
+        <h5 className="mt-6 text-[20px] font-bold text-cyan-600 capitalize">
           {postDataRef.current.title}
         </h5>
-        <span className="mt-3 mb-6 block leading-[1.5] font-light whitespace-pre-wrap">
+        <span className="mb-6 block leading-[1.5] font-light whitespace-pre-wrap">
           {postDataRef.current.content}
         </span>
-        <div className="mb-2 flex flex-row items-center gap-x-2">
-          <Calendar className="opacity-50" size={24}></Calendar>
-          <span className="text-base font-light opacity-65">Feb 6, 2025</span>
-        </div>
-        <div className="flex flex-row items-center gap-x-2">
-          <Map className="opacity-50" size={24}></Map>
-          <span className="text-base font-light opacity-65">
-            {postDataRef.current.location}
-          </span>
+        <div className="flex">
+          <div className="basis-1/2">
+            {/* <span className="font-regular text-base underline opacity-65">
+              Event Details
+            </span> */}
+            {/* <div className="mt-2 mb-2 flex flex-row items-center gap-x-2">
+              <Calendar className="opacity-50" size={24}></Calendar>
+              <span className="text-base font-light opacity-65">
+                Feb 6, 2025
+              </span>
+            </div> */}
+            <div className="mb-2 flex flex-row items-center gap-x-2">
+              <Map className="opacity-50" size={24}></Map>
+              <span className="text-base font-light opacity-65">
+                {postDataRef.current.location}
+              </span>
+            </div>
+          </div>
+          {/* <div className="basis-1/2">
+            <span className="font-regular text-base underline opacity-65">
+              Course Details
+            </span>
+            <div className="mt-2 mb-2 flex flex-row items-center gap-x-2">
+              <LibraryBig className="opacity-50" size={24}></LibraryBig>
+              <span className="text-base font-light opacity-65">
+                Interactive Design
+              </span>
+            </div>
+            <div className="flex flex-row items-center gap-x-2">
+              <BookOpen className="opacity-50" size={24}></BookOpen>
+              <span className="text-base font-light opacity-65">CPRG-309</span>
+            </div>
+          </div> */}
         </div>
       </div>
     );
